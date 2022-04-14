72a97e87cf6ab7f9bde161c85d4d8915342e5788
Iyere Handsome(Beau) Omogbeme <42305168+beauspot@users.noreply.github.com>
2022-04-14 18:08:38 +0100

    fixed the bug in the controllers/user.js file then proceeded to comment out the import user obj in line 2 in the same module.

---
+++ controllers/users.js
@@ -0,0 +1,47 @@
+import { v4 as uuidv4 } from "uuid";
+//import { users } from "../usersdb.js";
+let users = [];
+
+export const getUser = (req, res) => {
+  res.send(users);
+};
+
+export const createUser = (req, res) => {
+  const user = req.body;
+  /*   
+    const userId = uuidv4();
+    const userWithId = { ...user, id: userId };
+    users.push(userWithId);  
+    <== for ref purpose ==>
+  */
+  users.push({ ...user, id: uuidv4() });
+  res.send(
+    `User with the name ${user.firstName} & ${user.lastName} was added to the database!`
+  );
+};
+
+export const getSpecificUser = (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+};
+
+export const deleteUser = (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+  res.send(`The User with the id ${id} has been deleted from the database.`);
+};
+
+export const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
+  const user = users.find((user) => user.id === id);
+
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
+
+  res.send(`The user with the id ${id} has been updated!`);
+};
--- routes/users.js
+++ routes/users.js
@@ -1,58 +1,29 @@
-import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import express from "express"; 
+
+import {
+  getUser,
+  createUser,
+  getSpecificUser,
+  deleteUser,
+  updateUser,
+} from "../controllers/users.js";
 
 // init express router
 const router = express.Router();
 
-// users db
-let users = [];
-
 // all routes in here starts with /users
-router.get("/", (req, res) => {
-  res.send(users);
-});
+router.get("/", getUser);
 
 // creating & Adding users to our db
-router.post("/", (req, res) => {
-  const user = req.body;
-
-  // generating a user id
-  const userID = uuidv4();
-  const userWithID = { ...user, id: userID };
-  users.push(userWithID);
-
-  res.send(
-    `The User with the name ${user.firstName}, the last name of ${user.lastName} has been added to the database!`
-  );
-});
+router.post("/", createUser);
 
 // getting a specific user
-router.get("/:id", (req, res) => {
-  const { id } = req.params;
-
-  const foundUser = users.find((user) => user.id === id);
-
-  res.send(foundUser);
-});
+router.get("/:id", getSpecificUser);
 
 // deleting a user
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  users = users.filter((user) => user.id !== id);
-  res.send(`The User with the id ${id} has been deleted from the database.`);
-});
+router.delete("/:id", deleteUser);
 
 // updating auser data
-router.patch("/:id", (req, res) => {
-  const { id } = req.params;
-  const { firstName, lastName, age } = req.body;
-  const user = users.find((user) => user.id === id);
-
-  if (firstName) user.firstName = firstName;
-  if (lastName) user.lastName = lastName;
-  if (age) user.age = age;
-
-  res.send(`The user with the id ${id} has been updated!`);
-});
+router.patch("/:id", updateUser);
 
 export default router;
