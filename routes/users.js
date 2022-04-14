66303da559d7f3d88b84ba709c7c588eea44f6d3
Iyere Handsome(Beau) Omogbeme <42305168+beauspot@users.noreply.github.com>
2022-04-14 12:35:38 +0100

    Imolement the users db module, and importing it to the routes/users.js module to be used.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 import express from "express";
+import UsersDB from "../usersdb.js"
 // init express router
 const router = express.Router();
 
+
+
 // all routes in here starts with /users
 router.get("/", (req, res) => {
+  console.log(UsersDB);
   res.send("Testing users");
 });
 
