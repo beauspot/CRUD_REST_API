ff5c263ed2042e19b7ebbd097888b3295153a4e2
Iyere Handsome(Beau) Omogbeme <42305168+beauspot@users.noreply.github.com>
2022-04-14 13:27:15 +0100

    removed the userdb.js module from the staging area & implemented post route.

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,37 @@
 import express from "express";
-import UsersDB from "../usersdb.js"
+import { v4 as uuidv4 } from "uuid";
+
 // init express router
 const router = express.Router();
 
+// users db
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
 
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 24,
+  },
+];
 
 // all routes in here starts with /users
 router.get("/", (req, res) => {
-  console.log(UsersDB);
-  res.send("Testing users");
+  res.send(users);
+});
+
+// Adding users to our db
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+
+  res.send(
+    `The User with the name ${user.firstName}, the last name of ${user.lastName} has been aded to the database!`
+  );
 });
 
 export default router;
